e59ea2d3a06d3e5a1396214bc47f45790acbfc59
belf33 <d16m10g33>
2022-09-15 14:45:04 +0300

    set up framework

--- tests/e2e/specs/login.spec.js
+++ tests/e2e/specs/login.spec.js
@@ -1,17 +1,49 @@
 describe('Test User Login', () => {
 
-    
-    it('Connects with Metamask', () => {
-        cy.visit('https://dappify.com')
-        cy.contains('Sign').click(); 
-        cy.contains('Confirm').click();
-        cy.contains('Connect Wallet').click();
-        cy.contains('Metamask').click();
-        cy.switchToMetamaskWindow();
-        cy.acceptMetamaskAccess().should("be.true");
-        cy.confirmMetamaskSignatureRequest();
-        cy.switchToCypressWindow();
-        cy.contains('Add Project').should('be.visible');
+	it('Add Mumbai network', () => {
+		cy.addMetamaskNetwork({
+			networkName: 'Mumbai Testnet',
+			rpcUrl: 'https://rpc-mumbai.maticvigil.com',
+			chainId: '80001',
+			symbol: 'ETH',
+			blockExplorer: 'https://explorer-mumbai.maticvigil.com',
+			isTestnet: true
+		});
+		cy.changeMetamaskNetwork("Mumbai");
+	});
 
-    })
-  })
+	it('Add Polygon network', () => {
+		cy.addMetamaskNetwork({
+			networkName: 'Polygon',
+			rpcUrl: 'https://rpc.ankr.com/polygon/486f6d938d85e35aeacf83a59afd95c4fab739093c8f919adb258799d81d51bf',
+			chainId: '137',
+			symbol: 'MATIC',
+			blockExplorer: 'https://polygonscan.com',
+			isTestnet: false
+		});
+	});
+
+	it('Connects with Metamask', () => {
+		cy.visit('https://wallet-new.polygon.technology');
+		// cy.addMetamaskNetwork("mumbai");
+		// cy.contains('Sign').click();
+		// cy.contains('Confirm').click();
+		// cy.contains('Connect Wallet').click();
+		// cy.contains('Metamask').click();
+		// cy.switchToMetamaskWindow();
+		// cy.addMetamaskNetwork({
+		// 	networkName: 'Mumbai Testnet',
+		// 	rpcUrl: 'https://rpc-mumbai.maticvigil.com',
+		// 	chainId: '80001',
+		// 	symbol: 'ETH',
+		// 	blockExplorer: 'https://explorer-mumbai.maticvigil.com',
+		// 	isTestnet: true
+		// });
+		// cy.changeMetamaskNetwork("Mumbai");
+		// cy.acceptMetamaskAccess().should("be.true");
+		// cy.confirmMetamaskSignatureRequest();
+		// cy.switchToCypressWindow();
+		// cy.contains('Add Project').should('be.visible');
+	});
+
+});
